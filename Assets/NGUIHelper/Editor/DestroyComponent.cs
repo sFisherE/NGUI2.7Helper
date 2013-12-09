4b86e2d3b7b48796645c8035af86f8e91ec055ce
sFisherE <397381741@qq.com>
2013-12-09 17:38:57 +0800

    test

---
+++ Assets/NGUIHelper/Editor/DestroyComponent.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+using UnityEngine;
+using UnityEditor;
+
+public class DestroyComponent : ScriptableWizard
+{
+
+    [MenuItem("UIEditTool/Destroy Component")]
+    static void CreateWizard()
+    {
+        ScriptableWizard.DisplayWizard("Destroy Component", typeof(DestroyComponent), "Close", "Destroy");
+    }
+
+
+}
