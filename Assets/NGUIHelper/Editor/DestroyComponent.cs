c5a680d46bc8d43b5856b4bf6c8b2f3ce695555c
sFisherE <397381741@qq.com>
2013-12-10 18:13:26 +0800

    doing

--- Assets/NGUIHelper/Editor/DestroyComponent.cs
+++ Assets/NGUIHelper/Editor/DestroyComponent.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 
 using UnityEngine;
+using System.Reflection;
 using UnityEditor;
 
 public class DestroyComponent : ScriptableWizard
@@ -13,5 +14,90 @@ public class DestroyComponent : ScriptableWizard
         ScriptableWizard.DisplayWizard("Destroy Component", typeof(DestroyComponent), "Close", "Destroy");
     }
 
+#region TypeInit
+    public DestroyComponent()
+    {
+        FillTypeList();
+    }
+    private List<Type> mTypes = new List<Type>();
+    void FillTypeList()
+    {
+        AppDomain domain = AppDomain.CurrentDomain;
+        Type ComponentType = typeof(Component);
+        mTypes.Clear();
+        foreach (Assembly asm in domain.GetAssemblies())
+        {
+            Assembly currentAssembly = null;
+            //	add UnityEngine.dll component types
+            if (asm.FullName == "UnityEngine")
+                currentAssembly = asm;
+            //	check only for temporary assemblies (i.e. d6a5e78fb39c28ds27a1ec4f9g1 )
+            if (ContainsNumbers(asm.FullName))
+                currentAssembly = asm;
+            if (currentAssembly != null)
+            {
+                foreach (Type t in currentAssembly.GetExportedTypes())
+                {
+                    if (ComponentType.IsAssignableFrom(t))
+                    {
+                        mTypes.Add(t);
+                    }
+                }
+            }
+        }
+    }
+    bool ContainsNumbers(String text)
+    {
+        int i = 0;
+        foreach (char c in text)
+        {
+            if (int.TryParse(c.ToString(), out i))
+                return true;
+        }
+        return false;
+    }
+#endregion
+
+    public string componentName;
+    public UnityEngine.Object folder;
+    public string path;
+    void OnWizardUpdate()
+    {
+        if (folder != null)
+            path = AssetDatabase.GetAssetPath(folder);
+        else
+            path = string.Empty;
+
+        Type t = GetSelectedType();
+        if (t == null)
+            errorString = "Type doesnt exist.";
+        else
+        {
+            errorString = string.Empty;
+        }
+        if (mTypes.Count == 0)
+            errorString = "Typelist is empty.";
+
+    }
+    void OnWizardOtherButton()
+    {
+        
+    }
+
+    Type GetSelectedType()
+    {
+        //Debug.Log(mTypes.Count);
+        foreach (Type t in mTypes)
+        {
+            //Debug.Log(t.Name);
+            if (t.Name == componentName)
+            {
+                Debug.Log("find the target "+componentName);
+                return t;
+            }
+        }
+        Debug.Log("can't find the target "+componentName);
+        return null;
+    }
 
 }
